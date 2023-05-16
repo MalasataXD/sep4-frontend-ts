b0c5fe467c9823a7b5e0395bc90aa7c50a7f0330
Mads Hylleberg Nielsen <madshyllebergn@gmail.com>
2023-05-16 12:48:14 +0200

    Improved EditValues with CSS Module + Restyling
    This improvement makes use of CSS module to avoid stylesheets having conflicts with each other later on. Also updated the way the ErrorState was shown/hidden. + A restyling of the entire component
    
    * ADDED
    - toggleErrorState method
    - Icons next to the input fields
    - Icon to the save button
    - CSS Module
    
    * REMOVED
    - showErrorState method
    - hideErrorState method
    - CSS stylesheet. (Replaced by CSS Module)
    
    * UPDATED
    - Return statement (Restructure of the HTML)

--- src/components/EditValues/EditValues.tsx
+++ src/components/EditValues/EditValues.tsx
@@ -1,6 +1,11 @@
 import { LINK, EditValuesPost } from "../config"
 import { useState } from "react";
-import "./EditValues.css"
+import styles from "./EditValues.module.css";
+
+// # ICONS
+import sendIcon from "../../img/icons/send.png";
+import temperatureIcon from "../../img/icons/temperature.png";
+import humidityIcon from "../../img/icons/humidity.png";
 
 export function EditValues() {
 
@@ -8,26 +13,43 @@ export function EditValues() {
     const [ temperature, setTemperature ] = useState<null | number>(null);
     const [ humidity, setHumidity ] = useState<null | number>(null);
     const [ errorState, setErrorState ] = useState("");
-
+    let hideErrorState : boolean = true;
 
     return (
-        <div className="Edit-values-card">
-            <div className="Title"><b>Edit Values</b></div>
-            <br/>
-            <div className="Input-container">
-                <input className="Input" placeholder="Temperature (°C)" type="number" onChange={
-                    (event) => setTemperature(event.target.value === "" ? null : Number(event.target.value))}/>
-                
-                <input className="Input" placeholder="Humidity (%)" type="number" onChange={
-                    (event) => setHumidity(event.target.value === "" ? null : Number(event.target.value))} />
-                
-                <button className="Save-button" onClick={() => handleClick()}> SAVE </button>
+        <div className={styles.card}>
+            <div className={styles.title}>
+                <b>Edit Values</b>
             </div>
-            <div className="error-container hide" id="errorState">{errorState}</div>
+            <div className={styles.container}>
+                <div className={styles.section}>
+                    <img className={styles.icon} src={temperatureIcon} alt="flame"/>
+                    <input className={styles.inputField} placeholder="Temperature (°C)" type="number" onChange={
+                        (event) => setTemperature(event.target.value === "" ? null : Number(event.target.value))}/>
+                </div>
+                <div className={styles.section}>
+                    <img className={styles.icon} src={humidityIcon} alt="water drop"/>
+                    <input className={styles.inputField} placeholder="Humidity (%)" type="number" onChange={
+                        (event) => setHumidity(event.target.value === "" ? null : Number(event.target.value))} />
+                </div>
+                <div className={styles.section}>
+                    <button className={styles.SaveButton} onClick={() => handleClick()}>
+                        <img className={styles.icon} src={sendIcon} alt="paper airplane"/>
+                        <p>SAVE</p>
+                    </button>
+                </div>
+            </div>
+            <div className={`${styles.ErrorContainer} ${styles.hide}`} id="errorState">{errorState}</div>
         </div>
     )  
 
-
+    // # Toggles the Error message at the bottom of the component.
+    function toggleErrorState()
+    {
+        const connectionSection = document.getElementById('errorState');
+        if (connectionSection) {
+            connectionSection.classList.toggle(`${styles.hide}`, hideErrorState);
+        }
+    }
     async function handleClick(){
         // Post to web-API
         if (validation()) {
@@ -43,46 +65,41 @@ export function EditValues() {
                 })
 
                 if (!response.ok) {
-                    showErrorState();
+                    hideErrorState = false;
+                    toggleErrorState();
                     setErrorState("Post failed!");
                 }
                 
             } catch (error) {
-                showErrorState();
+                hideErrorState = false;
+                toggleErrorState();
                 setErrorState("server didn't respond!");
             }         
         }
     }
-
     function validation(){
         if (temperature === null || humidity === null) {
-            showErrorState();
+            hideErrorState = false;
+            toggleErrorState();
             setErrorState("There must be an input");
             return false;
         }
         if (temperature > 100 || temperature < -20) {
-            showErrorState();
+            hideErrorState = false;
+            toggleErrorState();
             setErrorState("Temperature must be between -20 and 100");
             return false;
         }
         if (humidity > 100 || humidity < 0) {
-            showErrorState();
+            hideErrorState = false;
+            toggleErrorState();
             setErrorState("Humidity must be between 0 and 100");
             return false;
         }
-        hideErrorState()
+        hideErrorState = true;
+        toggleErrorState();
         setErrorState("")
         return true;
-    }
-
-    function hideErrorState() {
-        var element = document.getElementById("errorState");
-        element?.classList.add("hide");
-    }
-
-    function showErrorState() {
-        var element = document.getElementById("errorState");
-        element?.classList.remove("hide");
     }
 }
 
