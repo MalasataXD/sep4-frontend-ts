2af3473f50109819f4fb47a1408337ae64b7c094
Marcus de Place <marcus6001@live.dk>
2023-05-16 10:07:55 +0200

    Created TitleCards Component
    * ADDED
    TitleCard.tsx (html for creating a TitleCard)
    TitleCard.css(css for TitleCard, can be improved)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import "./App.css";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
 //test
 import LoginStaus from "./components/LoginStatus/LoginStatus";
 import LivePage from "./routes/LivePage/LivePage";
+import TitleCard from "./components/TitleCard/TitleCard";
 
 //note: navbar ned to uses outlet
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Navbar />}>
-      <Route index element={<LivePage />} />
+      <Route
+        index
+        element={
+          <div>
+            <TitleCard
+              title={"Livepage"}
+              description={
+                "Dette er et link til de nyeste værdier fra dit klima køle anlæg :)"
+              }
+              image={
+                "https://lh3.googleusercontent.com/pw/AJFCJaWn751DzAU67z_RAiGDpz0S_kLRahQ4TcLLNjkg7u3Y3Q6hGGCYMsIUTJgNWCjvlMlJi0c0eW6jXacPk_CKWmzi3aSM_kY-4kc3M4qcD3MWE6uZQkk=w2400"
+              }
+              link={"/test"}
+              color={"blue"}
+            />
+          </div>
+        }
+      />
       <Route path="/test" element={<LoginStaus />} />
     </Route>
   )
