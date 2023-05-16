3b7e0592bc44449a80bfe3315cc364c45255d3e7
Marcus de Place <marcus6001@live.dk>
2023-05-16 11:12:40 +0200

    Merged from dev

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,7 @@ import TitleCard from "./components/TitleCard/TitleCard";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Navbar />}>
-      <Route
-        index
-        element={
-          <div>
-            <TitleCard
-              title={"Livepage"}
-              description={
-                "Dette er et link til de nyeste værdier fra dit klima køle anlæg :)"
-              }
-              image={
-                "https://lh3.googleusercontent.com/pw/AJFCJaWn751DzAU67z_RAiGDpz0S_kLRahQ4TcLLNjkg7u3Y3Q6hGGCYMsIUTJgNWCjvlMlJi0c0eW6jXacPk_CKWmzi3aSM_kY-4kc3M4qcD3MWE6uZQkk=w2400"
-              }
-              link={"/test"}
-              color={"blue"}
-            />
-          </div>
-        }
-      />
+      <Route index element={<LivePage></LivePage>} />
       <Route path="/test" element={<LoginStaus />} />
     </Route>
   )
