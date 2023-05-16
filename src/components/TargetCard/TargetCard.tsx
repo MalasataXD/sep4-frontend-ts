c7f8e4048044797e7f2ced943f8293a5a2867150
Bjarke <bjarkebp@live.dk>
2023-05-16 11:34:20 +0200

    TargetCard test
    *Added
    -TargetCard.test.tsx
    -More validation

--- src/components/TargetCard/TargetCard.tsx
+++ src/components/TargetCard/TargetCard.tsx
@@ -1,6 +1,8 @@
 import "./TargetCard.css";
 import { LINK, TargetCardUpdate } from "../config";
 import { useState } from "react";
+import { Turn } from "hamburger-react";
+import { forEachChild } from "typescript";
 
 export default function TargetCard(props: any) {
   const [temperature, setTemperature] = useState<null | number>(null);
@@ -10,14 +12,14 @@ export default function TargetCard(props: any) {
   const [errorState, setErrorState] = useState("");
 
   return (
-    <div className="Edit-values-card">
-      <div className="Title">
-        <b>{props.title}</b>
+    <div className="Target-card-card">
+      <div className="Title-TargetCard">
+        <b>{props.Title}</b>
       </div>
       <br />
-      <div className="Input-container">
+      <div className="Input-container-TargetCard">
         <input
-          className="Input"
+          className="Input-TargetCard"
           placeholder="Temperature (°C)"
           type="number"
           onChange={(event) =>
@@ -28,7 +30,7 @@ export default function TargetCard(props: any) {
         />
 
         <input
-          className="Input"
+          className="Input-TargetCard"
           placeholder="Humidity (%)"
           type="number"
           onChange={(event) =>
@@ -39,9 +41,9 @@ export default function TargetCard(props: any) {
         />
 
         <input
-          className="Input"
-          placeholder="hh:mm:ss (%)"
-          type="string"
+          className="Input-TargetCard"
+          placeholder="Time (hh:mm:ss)"
+          type="text"
           onChange={(event) =>
             setTime(event.target.value === "" ? null : event.target.value)
           }
@@ -56,9 +58,12 @@ export default function TargetCard(props: any) {
   );
 
   function isWhereASaveButton() {
-    if (props.saveButton === true) {
+    if (props.isEditModeOn === true) {
       return (
-        <button className="Save-button" onClick={() => handleClick()}>
+        <button
+          className="Save-button-TargetCard"
+          onClick={() => handleClick()}
+        >
           SAVE
         </button>
       );
@@ -82,7 +87,7 @@ export default function TargetCard(props: any) {
         // -------------------------------------------------------------------
         if (!response.ok) {
           showErrorState();
-          setErrorState("Post failed!");
+          setErrorState("Update failed!");
         }
       } catch (error) {
         showErrorState();
@@ -117,22 +122,39 @@ export default function TargetCard(props: any) {
     //Test for time
     const Array = time.split(":");
 
-    if (Array[0] > "24" || Array[0] < "00") {
+    if (Array.length != 3) {
+      showErrorState();
+      setErrorState("Time is not the right format");
+      return false;
+    }
+
+    if (parseInt(Array[0]) > 24 || parseInt(Array[0]) < 0) {
       showErrorState();
       setErrorState("Hours must be between 00 and 24");
       return false;
     }
 
-    if (Array[1] > "60" || Array[1] < "00") {
+    if (parseInt(Array[1]) > 60 || parseInt(Array[1]) < 0) {
       showErrorState();
       setErrorState("Minutes must be between 00 and 60");
       return false;
     }
 
-    if (Array[2] > "60" || Array[2] < "00") {
+    if (parseInt(Array[2]) > 60 || parseInt(Array[2]) < 0) {
       showErrorState();
       setErrorState("Seconds must be between 00 and 60");
       return false;
+    }
+
+    for (let i = 0; i < Array.length; i++) {
+      for (let j = 0; i < Array[i].length; j++) {
+        console.log(isNaN(parseInt(Array[i][j])));
+        if (isNaN(parseInt(Array[i][j]))) {
+          showErrorState();
+          setErrorState("Not a number");
+          return false;
+        }
+      }
     }
 
     hideErrorState();
